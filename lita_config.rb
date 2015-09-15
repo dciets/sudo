dc6ab79e5ea9ef256d8bc5f9fade1d7c784991a9
Emile Filteau-Tessier <emilefilteau-tessier@Emiles-MacBook-Pro.local>
2015-09-15 10:22:12 -0400

    Added Gemfile.lock

--- lita_config.rb
+++ lita_config.rb
@@ -1,38 +1,19 @@
 Lita.configure do |config|
-  # The name your robot will use.
-  config.robot.name = "Lita"
+  config.robot.name = "sudo"
 
-  # The locale code for the language to use.
-  # config.robot.locale = :en
-
-  # The severity of messages to log. Options are:
-  # :debug, :info, :warn, :error, :fatal
-  # Messages at the selected level and above will be logged.
   config.robot.log_level = :info
 
-  # An array of user IDs that are considered administrators. These users
-  # the ability to add and remove other users from authorization groups.
-  # What is considered a user ID will change depending on which adapter you use.
-  config.robot.admins = ["U04C6MLDY"]
+  config.robot.admins = [ENV['SLACK_ADMIN_ID']]
 
-  # The adapter you want to connect with. Make sure you've added the
-  # appropriate gem to the Gemfile.
   config.robot.adapter = :slack
-
-  config.adapters.slack.token = "xoxb-10738878231-VOVUpkUqJmFdK8ADrnwfVCDG"
+  config.adapters.slack.token = ENV['SLACK_TOKEN']
 
   config.redis[:url] = ENV["REDISTOGO_URL"]
-  config.http.port = ENV["PORT"]
+  config.http.port = 1234 # ENV["PORT"]
 
-  ## Example: Set options for the chosen adapter.
-  # config.adapter.username = "myname"
-  # config.adapter.password = "secret"
-
-  ## Example: Set options for the Redis connection.
-  # config.redis.host = "127.0.0.1"
-  # config.redis.port = 1234
-
-  ## Example: Set configuration for any loaded handlers. See the handler's
-  ## documentation for options.
-  # config.handlers.some_handler.some_config_key = "value"
+  config.handlers.google_images.google_cse_id = ENV['CSE_ID']
+  config.handlers.google_images.google_cse_key = ENV['GOOGLE_API_KEY']
+  config.handlers.timezone.default_zone = 'Canada/Eastern'
+  config.handlers.youtube_me.api_key = ENV['GOOGLE_API_KEY']
+  config.handlers.youtube_me.top_result = true
 end
