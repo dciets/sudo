675019c71856b8293d9f836318bbdd4e786471a9
Emile Filteau <emile.filteau@gmail.com>
2016-04-15 11:30:55 -0400

    Added lennyorgy

--- common.rb
+++ common.rb
@@ -3,6 +3,10 @@ class Common < Lita::Handler
     response.reply('( ͡° ͜ʖ ͡°)')
   end
 
+  route(/^lennyorgy$/i) do |response|
+    response.reply('( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)')
+  end
+
   route(/^montre nous tes fesses$/i, command: true) do |response|
     response.reply('( ＾◡＾)っ (‿|‿)')
   end
